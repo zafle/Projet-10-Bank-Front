d634a9e42a2dc28f1ba9b1bc2a8f8b0212d5967e
zafle <fleutotanne@free.fr>
2025-01-14 14:27:42 +0100

    rename index.css to main.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 import Loader from './components/loader/Loader'
-import './index.css'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
